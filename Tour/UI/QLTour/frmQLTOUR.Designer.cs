713b07a81035c7dfc9805efab69b74eb5ed70ec9
Ngọc Nguyên <doctorsuperhero13@gmail.com>
2021-12-31 11:11:03 +0700

    FINALLLLLLLLLL

--- Tour/UI/QLTour/frmQLTOUR.Designer.cs
+++ Tour/UI/QLTour/frmQLTOUR.Designer.cs
@@ -29,6 +29,7 @@ namespace Tour.UI.QLTour
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.comboBox_DiaDiem = new System.Windows.Forms.ComboBox();
             this.comboBox_LoaiHinh = new System.Windows.Forms.ComboBox();
             this.txtDD = new System.Windows.Forms.TextBox();
@@ -151,6 +152,14 @@ namespace Tour.UI.QLTour
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
